b055d0e36ccf4f957a024a433f3fea142f8a1101
zaher88abd <zaher88abd@gmail.com>
2018-06-04 14:56:19 -0300

    test Hooks error

--- src/main/java/HelloWord.java
+++ src/main/java/HelloWord.java
@@ -5,5 +5,6 @@ public class HelloWord {
         System.out.println("HelloWord");
         System.out.println("HelloWord");
         System..println("HelloWord");
+        System..println("HelloWord");
     }
 }
