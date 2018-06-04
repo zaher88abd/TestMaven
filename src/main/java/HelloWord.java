fa7c902a70a53ef545da940b82cd370a6760d339
zaher88abd <zaher88abd@gmail.com>
2018-06-04 16:09:24 -0300

    fix

--- src/main/java/HelloWord.java
+++ src/main/java/HelloWord.java
@@ -1,10 +1,10 @@
+import org.springframework.boot.context.annotation.Configurations;
+
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("HelloWord");
         System.out.println("HelloWord");
         System.out.println("HelloWord");
         System.out.println("HelloWord");
-        System..println("HelloWord");
-        System..println("HelloWord");
     }
 }
