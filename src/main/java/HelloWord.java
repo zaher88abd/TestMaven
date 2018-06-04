5c86ca3c0d64319c6c07eca7ba53e290e8ab4ee7
zaher88abd <zaher88abd@gmail.com>
2018-06-04 12:52:06 -0300

    test Hooks

--- src/main/java/HelloWord.java
+++ src/main/java/HelloWord.java
@@ -2,5 +2,6 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("HelloWord");
         System.out.println("HelloWord");
+        System.out.println("HelloWord");
     }
 }
